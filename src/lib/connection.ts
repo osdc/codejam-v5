0a7f24ba26669f912439987a7e3beb58e149f152
sh66n <shaanedroos85@gmail.com>
2024-12-26 23:29:59 +0530

    fixed build issues #11

--- src/lib/connection.ts
+++ src/lib/connection.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectToDb = async () => {
+  const connUrl = process.env.MONGO_CONNECTION_URL as string;
   try {
-    await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+    await mongoose.connect(connUrl);
   } catch (e: unknown) {
     if (e instanceof Error) throw new Error(e.message);
     else throw new Error("An unknown error occurred");
