4d35a7e48664c9a1fc9f77305411e24ef7b39ee6
sh66n <shaanedroos85@gmail.com>
2024-12-27 15:55:05 +0530

    debugging /items responding with 401 #2

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,5 +1,7 @@
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NODE_ENV === "production") {
+    return "https://jiit-lost-and-found.vercel.app"; // Replace with your actual URL
+  }
   return process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000"; // dev SSR should use localhost
 };
